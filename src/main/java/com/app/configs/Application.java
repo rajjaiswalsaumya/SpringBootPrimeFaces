91038b185927a876c54d014f21ad16cc88d4a6f2
rajjaiswalsaumya <rajjaiswalsaumya@gmail.com>
2014-09-03 07:10:59 +0530

    Removed Wildfly dependency

--- src/main/java/com/app/configs/Application.java
+++ src/main/java/com/app/configs/Application.java
@@ -1,39 +1,32 @@
-/**
- *  (C) 2013-2014 Stephan Rauh http://www.beyondjava.net
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.app.configs;
 
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
-
+import com.sun.faces.config.ConfigureListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import com.sun.faces.config.ConfigureListener;
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application implements ServletContextAware {
+public class Application extends SpringBootServletInitializer implements ServletContextAware {
+    private static Log logger = LogFactory.getLog(Application.class);
+
+    public Application() {
+        logger.debug("Initialised Application");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
@@ -42,7 +35,7 @@ public class Application implements ServletContextAware {
     @Bean
     public ServletRegistrationBean facesServletRegistration() {
         ServletRegistrationBean registration = new ServletRegistrationBean(
-                new FacesServlet(),  "*.xhtml", "*.jsf");
+                new FacesServlet(), "*.xhtml", "*.jsf");
         registration.setLoadOnStartup(1);
         return registration;
     }
@@ -51,6 +44,14 @@ public class Application implements ServletContextAware {
     public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
         return new ServletListenerRegistrationBean<ConfigureListener>(
                 new ConfigureListener());
+    }
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsf");
+        return resolver;
     }
 
     @Override
